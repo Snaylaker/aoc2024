6689a48fcc5c908f52364adbbfac344c7e2d363f
Snaylaker <mehdihimmid73@gmail.com>
2024-12-01 14:55:22 +0100

    day1: there was a part two oups

--- day1.js
+++ day1.js
@@ -9,6 +9,24 @@ function distanceBetweenTwoLists(list1, list2) {
   }
   return result;
 }
+/**
+ * @param {Array<Number>} list1
+ * @param {Array<Number>} list2
+ */
+function similarityScore(list1, list2) {
+  list2.sort();
+  let result = 0;
+  list1.forEach((el1) => {
+    let counter = 0;
+    list2.forEach((el2) => {
+      if (el1 === el2) {
+        counter++;
+      }
+    });
+    result += el1 * counter;
+  });
+  return result;
+}
 
 fs.readFile("input.txt", "utf8", (err, data) => {
   if (err) {
@@ -24,5 +42,6 @@ fs.readFile("input.txt", "utf8", (err, data) => {
     list1.push(Number.parseInt(splitedLine[0]));
     list2.push(Number.parseInt(splitedLine[1]));
   });
-  console.log(distanceBetweenTwoLists(list1, list2));
+  console.log("distance :", distanceBetweenTwoLists(list1, list2));
+  console.log("similarity: ", similarityScore(list1, list2));
 });
